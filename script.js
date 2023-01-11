40dc49eaaaef0d64f999ad554706fc185dc2771b
Adrian Kurek <adrian.kurek93@gmail.com>
2023-01-11 12:33:08 +0100

    Change the buttons look and feel

--- script.js
+++ script.js
@@ -9,9 +9,9 @@
                         <p class="card-text">${d.name}. ${d.description}</p>
                         <div class="d-flex justify-content-between align-items-center">
                             <div class="btn-group">
-                                <a class="btn btn-sm btn-outline-secondary" target='_blank'href="https://www.google.com/maps/search/${d.latitude},+${d.longitude}">Zobacz mapę</a>
-                                <a class="btn btn-sm btn-outline-secondary" href="./images.html?zapadlisko=${d.query}">Więcej zdjęć</a>
-                                <a class="btn btn-sm btn-outline-secondary" href="./media.html?zapadlisko=${d.query}">Media</a>
+                                <a class="btn btn-sm btn-outline-primary" target='_blank'href="https://www.google.com/maps/search/${d.latitude},+${d.longitude}">Zobacz mapę</a>
+                                <a class="btn btn-sm btn-outline-success" href="./images.html?zapadlisko=${d.query}">Więcej zdjęć</a>
+                                <a class="btn btn-sm btn-outline-danger" href="./media.html?zapadlisko=${d.query}">Media</a>
                             </div>
                             <small class="text-muted">${d.date}</small>
                         </div>
