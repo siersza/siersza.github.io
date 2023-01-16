72fb7cf7997b0b8d17ed93633c5c251536e1fca4
Adrian Kurek <adrian.kurek93@gmail.com>
2023-01-16 16:35:30 +0100

    Replace the image source if it failed to load

--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,13 @@
 import { depressions } from "../data/depressions.js";
 
 export const getDepressionByQuery = (query) => depressions.filter(d => d.query === query)[0];
+
+// Replace the src if the image failed to load (it does not exist, there is no access to it etc.).
+export const replaceImgSrc = (images) => {
+    for (let i = 0; i < images.length; i++) {
+        images[i].onerror = () => {
+            images[i].src = '../images/image-placeholder.png';
+            images[i].style.pointerEvents = 'none';
+        };
+    }
+}
