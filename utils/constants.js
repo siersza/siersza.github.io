4944b4294dab963403c8f16cca40e900df775cf2
Adrian Kurek <adrian.kurek93@gmail.com>
2023-01-17 09:05:19 +0100

    Add a new page for video resources

--- utils/constants.js
+++ utils/constants.js
@@ -2,10 +2,12 @@ const DEFAULT_TITLE = 'Strona o sierszańskich zapadliskach';
 const NO_MEDIA_ERROR = 'Na ten moment nie ma odnośników dotyczących zapadliska. :(';
 const NO_IMAGES_ERROR = 'Na ten moment nie ma zdjęć dotyczących tego zapadliska. :(';
 const COULD_NOT_LOAD_IMAGE = 'Nie udało się załadować zdjęcia. Zostało usunięte lub nie ma do niego dostępu :(';
+const VIDEOS_PAGE_TITLE = 'Materiały wideo dotyczące zapadlisk';
 
 export {
     DEFAULT_TITLE,
     NO_MEDIA_ERROR,
     NO_IMAGES_ERROR,
-    COULD_NOT_LOAD_IMAGE
+    COULD_NOT_LOAD_IMAGE,
+    VIDEOS_PAGE_TITLE
 }
