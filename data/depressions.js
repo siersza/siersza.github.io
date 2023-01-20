a1bec3bd9889b5203dd0270cf1ddca8e370a822f
siersza <64686620+siersza@users.noreply.github.com>
2023-01-20 16:15:45 +0100

    Update depressions.js

--- data/depressions.js
+++ data/depressions.js
@@ -12,8 +12,8 @@ export const depressions = [
         media: [
             "https://przelom.pl/44428-zapadla-sie-ziemia-przy-cmentarzu-w-trzebini.html?fbclid=IwAR0XS8KYjGYyulsTkkpOAOEd_DDOkI4EZ2fbR7avCrNpRGtYSQcVYyY7f60"
         ],
-        latitude: "",
-        longitude: "",
+        latitude: "50.19821899033548",
+        longitude: "19.44042191115233",
         diameter: "b.d.",
         depth: "b.d."
     },
