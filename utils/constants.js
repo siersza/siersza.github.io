d1ee68079f024a4db8ef1e0e8d9531eee6c06eac
siersza <64686620+siersza@users.noreply.github.com>
2023-02-04 11:10:40 +0100

    Update constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,6 @@
 const DEFAULT_TITLE = 'Strona główna';
 const DEFAULT_DESCRIPTION = 'Strona poświęcona występującym w Sierszy zapadliskom';
-const NO_DEPRESSION_ERROR = 'Nie znaleziono takiego znaleziska.';
+const NO_DEPRESSION_ERROR = 'Nie znaleziono takiego zapadliska.';
 const NO_MEDIA_ERROR = 'Na ten moment nie ma odnośników do artykułów dotyczących zapadliska.';
 const NO_IMAGES_ERROR = 'Na ten moment nie ma zdjęć dotyczących tego zapadliska.';
 const COULD_NOT_LOAD_IMAGE_ERROR = 'Nie udało się załadować zdjęcia. Zostało usunięte lub nie ma do niego dostępu';
