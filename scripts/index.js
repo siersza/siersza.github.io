cc650d045f1fbcdac471165f7687d53e779c3f30
Adrian Kurek <adrian.kurek93@gmail.com>
2023-04-07 17:08:11 +0200

    Implement hash routing

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+// TODO: Refactor.
 import * as CONSTANT from "../utils/constants.js";
 import { renderCard, renderURL, renderImage, renderGalleryImage, renderErrorMessage } from "./renderer.js";
 import { depressions } from "../data/depressions.js";
@@ -6,109 +7,105 @@ import { videos } from "../data/videos.js";
 import { memes } from "../data/memes.js";
 import { setPageTitle, setPageDescription, setDocumentTitle, setActivePage, getDepressionById } from "../utils/utils.js";
 
-const routes = {
-    "/": {
-        renderer: home,
-        page: 'home',
-        title: CONSTANT.DEFAULT_TITLE,
-        description: `${CONSTANT.DEFAULT_DESCRIPTION}. Na dzień dzisiejszy strona zawiera informacje na temat <b>${depressions.length}</b> zapadlisk.`,
-        documentTitle: 'Trzebinia Siersza | Zapadliska'
-    },
-    "/pages/about": {
-        renderer: about,
-        page: 'about',
-        title: CONSTANT.ABOUT_PAGE_TITLE,
-        description: CONSTANT.ABOUT_PAGE_DESCRIPTION,
-        documentTitle: 'Trzebinia Siersza | O Zapadliskach'
-    },
-    "/pages/video": {
-        renderer: video,
-        page: 'video',
-        title: CONSTANT.VIDEOS_PAGE_TITLE,
-        description: '',
-        documentTitle: 'Trzebinia Siersza | Materiały Wideo'
-    },
-    "/pages/meme": {
-        renderer: meme,
-        page: 'meme',
-        title: CONSTANT.MEMES_PAGE_TITLE,
-        description: CONSTANT.MEMES_PAGE_DESCRIPTION,
-        documentTitle: 'Trzebinia Siersza | Memy'
-    },
-    "/pages/gallery/depression": {
-        renderer: gallery,
-        page: '',
-        title: 'Zdjęcia dotyczące zapadliska:',
-        description: '',
-        documentTitle: 'Galeria |'
-    },
-    "/pages/media/depression": {
-        renderer: urls,
-        page: '',
-        title: 'Artykuły dotyczące zapadliska:',
-        description: '',
-        documentTitle: 'Media |'
-    }
-};
-
 const mediaContainer = document.getElementById('media-container');
 const imagesContainer = document.getElementById('images-container');
 
-window.route = route;
-window.onpopstate = handleRoute;
+function handleRouteChange() {
+    const route = window.location.hash.substr(1);
+    const params = getQueryParams(route);
 
-handleRoute();
+    clearContainers();
+
+    if (route === '') {
+        renderHome();
+    }
+
+    if (route === '/pages/about') {
+        renderAbout();
+    }
+
+    if (route === '/pages/video') {
+        renderVideos();
+    }
+
+    if (route === '/pages/meme') {
+        renderMemes();
+    }
+
+    if (route.includes('/pages/gallery/depression')) {
+        if (params === undefined || params.size === 0) {
+            return;
+        }
+
+        renderGallery(getDepressionById(params['id']));
+    }
+
+    if (route.includes('/pages/media/depression')) {
+        if (params === undefined) {
+            return;
+        }
+
+        renderUrls(getDepressionById(params['id']));
+    }
+}
+
+handleRouteChange();
+
+function getQueryParams(url) {
+    if (!url.includes('?')) {
+        return;
+    }
+
+    const params = [];
+    const queryString = url.split('?')[1];
+    const queryParams = queryString.split('&');
+
+    queryParams.forEach(param => {
+        const [key, value] = param.split('=');
+        params[key] = value;
+    });
+
+    return params;
+}
+
+window.route = route;
+window.onpopstate = handleRouteChange;
 
 function route(event) {
     event = event || window.event;
     event.preventDefault();
     window.history.pushState({}, '', event.target.href);
-    handleRoute();
+    handleRouteChange();
 }
 
-export function handleRoute() {
-    const path = window.location.pathname;
-    const id = window.location.search.split('=')[1];
-    const routeData = routes[path];
-    const render = routeData.renderer;
-    const depression = getDepressionById(id);
-
-    clearContainers();
-    render(depression);
-    setActivePage(routeData.page);
-    setPageTitle(depression === undefined ? routeData.title : `${routeData.title} ${depression.name}`);
-    setPageDescription(routeData.description);
-    setDocumentTitle(depression === undefined ? routeData.documentTitle : `${routeData.documentTitle} ${depression.name}`);
-}
-
-function home() {
+function renderHome() {
     depressions.forEach(d => imagesContainer.innerHTML += renderCard(d));
 }
 
-function about() {
+function renderAbout() {
     mediaContainer.innerHTML = `<p>${info.about}</p>`;
     info.urls.forEach(u => mediaContainer.innerHTML += renderURL(u));
     mediaContainer.innerHTML += renderImage("../../images/map.png");
     mediaContainer.innerHTML += '<iframe src="https://www.google.com/maps/d/embed?mid=1bbmXIbYZiiDqiAi9_VR9d22BVzNGTi4&ehbc=2E312F" height="700" style="margin-top: 30px"></iframe>';
 }
 
-function video() {
+function renderVideos() {
     videos.forEach(v => mediaContainer.innerHTML += renderURL(v));
 }
 
-function meme() {
+function renderMemes() {
     memes.forEach(meme => imagesContainer.innerHTML += renderGalleryImage(meme.href));
 }
 
-function gallery(depression) {
+function renderGallery(depression) {
     depression.images.forEach(image => imagesContainer.innerHTML += renderGalleryImage(image));
-    
+
     if (depression.images.length < 1) {
         mediaContainer.innerHTML = renderErrorMessage(CONSTANT.NO_IMAGES_ERROR);
     }
 }
 
-function urls(depression) {
+function renderUrls(depression) {
     depression.media.forEach(m => mediaContainer.innerHTML += renderURL(m));
 
     if (depression.media.length < 1) {
