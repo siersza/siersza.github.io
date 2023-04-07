e0e024786d13601da971de99302cb6be831391da
Adrian Kurek <adrian.kurek93@gmail.com>
2023-04-07 21:58:36 +0200

    Cosmetic changes

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -10,17 +10,17 @@ export const renderCard = depression => `
             <img class="card-img-top" src="${depression.imgSrc}"/>
             <div class="card-body">
                 <p class="card-text">${depression.name}. ${depression.description}</p>
-                <ul>
-                    <li>Głębokość: ${depression.depth} ${depression.depth === 'b.d.' ? '' : 'metry/ów.'}</li>
-                    <li>Średnica: ${depression.diameter} ${depression.diameter === 'b.d.' ? '' : 'metry/ów.'}</li>
+                <ul class="list-group">
+                    <li class="list-group-item">Data wystąpienia: ${depression.date}</li>
+                    <li class="list-group-item">Głębokość: ${depression.depth} ${depression.depth === 'b.d.' ? '' : 'metry/ów.'}</li>
+                    <li class="list-group-item">Średnica: ${depression.diameter} ${depression.diameter === 'b.d.' ? '' : 'metry/ów.'}</li>
                 </ul>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
+                <div style="margin-top: 16px">
+                    <div class="btn-group" style="width: 100%">
                         <a class="btn btn-sm btn-outline-primary" target='_blank' href="https://www.google.com/maps/search/${depression.latitude},+${depression.longitude}">Zobacz mapę</a>
                         <a class="btn btn-sm btn-outline-success" href="#/gallery/depression?id=${depression.id}" onclick="route()">Więcej zdjęć <strong>(${depression.images.length})</strong></a>
                         <a class="btn btn-sm btn-outline-danger" href="#/media/depression?id=${depression.id}" onclick="route()">Media <strong>(${depression.media.length})</strong></a>
                     </div>
-                    <small class="text-muted">${depression.date}</small>
                 </div>
             </div>
         </div>
